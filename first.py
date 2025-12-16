cd71d61b2270ef7669bc8ff7b1552247d6af5e0f
allyouare1 <242582@astanait.edu.kz>
2025-12-16 15:06:52 +0500

    Первый нормальный коммит моего python-проекта

---
+++ first.py
@@ -0,0 +1,9 @@
+print("hello")
+lastn=input("Your last name,")
+firstn=input("first name?")
+age=input ("How old are you?")
+phonenumber=input("Your phone number?")
+
+print (f"Your first name,last name: {firstn},{lastn}")
+print(f"Your age:{age}")
+print(f"Your phone number:{phonenumber}")
