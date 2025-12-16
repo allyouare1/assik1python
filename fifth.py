02fb6da0da6471baa195b16760c78bcc6c94916c
allyouare1 <242582@astanait.edu.kz>
2025-12-16 17:22:45 +0500

    guess a number

---
+++ fifth.py
@@ -0,0 +1,7 @@
+print("choose a number")
+print("multiply it by 5")
+print("add 8")
+print("multiply by 2")
+result = int(input("Your result: "))
+guessed_number = (result - 16) // 10
+print(f"Your number is : {guessed_number}")
