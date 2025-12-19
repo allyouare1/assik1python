490bec98e36ec8b675acc71d5a4fd4f7f3984767
allyouare1 <242582@astanait.edu.kz>
2025-12-19 18:09:34 +0500

    >>-->

---
+++ assik2/task1.py
@@ -0,0 +1,8 @@
+put=input()
+count=0
+for i in range (0, len(put)-4):
+    if (put[i]== ">"and  put[i+1]==">" and  put[i+2]=="-" and put[i+3]=="-" and put[i+4]==">"):
+        count+=1
+    elif (put[i]== "<" and  put[i+1]=="-" and  put[i+2]=="-" and put[i+3]=="<" and  put[i+4]=="<"):
+        count+=1
+print (count)
