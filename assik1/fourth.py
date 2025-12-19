6add5973d09e0a5c1232c8e27a179e7929608e19
allyouare1 <242582@astanait.edu.kz>
2025-12-19 18:18:55 +0500

    папка

--- assik1/fourth.py
+++ assik1/fourth.py
@@ -1,4 +1,3 @@
-
 n = int(input())
 total = sum(range(1, n + 1))
 print(total)
--- assik1/second.py
+++ assik1/second.py
@@ -1,9 +1,4 @@
 a, b, c = map(int, input().split())
-
-
-
 maximum = max(a, b, c)
 minimum = min(a, b, c)
-
-
 print(maximum - minimum)
