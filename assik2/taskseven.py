177eb8f17918438500f66eb204fcbfc87fc8be77
allyouare1 <242582@astanait.edu.kz>
2025-12-23 22:47:01 +0500

    apple

--- assik2/taskseven.py
+++ assik2/taskseven.py
@@ -1,20 +1,20 @@
-line = input().strip()
-items = line.split()
+l = input().strip()
+items = l.split()
 
-freq = {}
+f = {}
 for item in items:
-    if item in freq:
-        freq[item] += 1
+    if item in f:
+        f[item] += 1
     else:
-        freq[item] = 1
+        f[item] = 1
 
 print("Purchase frequency:")
-for item, count in freq.items():
+for item, count in f.items():
     print(f"{item}: {count}")
 
-m_item = None
+m_items= None
 m_count = 0
-for item, count in freq.items():
+for item, count in f.items():
     if count > m_count:
         m_count = count
         m_item = item
@@ -22,13 +22,12 @@ for item, count in freq.items():
 print("Most popular item:", m_item)
 
 o_items = []
-for item, count in freq.items():
+for item, count in f.items():
     if count == 1:
         o_items.append(item)
-
 print("Purchased once:", " ".join(o_items))
 
-sorted = list(freq.items())
+sorted = list(f.items())
 sorted.sort(key=lambda pair: pair[1], reverse=True)
 
 print("Sorted by frequency:")
