b20b89d850a8adc9d1dc901eb2627aa274999763
allyouare1 <242582@astanait.edu.kz>
2025-12-24 17:31:59 +0500

    rectangle area

---
+++ assik3/task22.py
@@ -0,0 +1,14 @@
+f1=int(input("Enter first rectangle first side: "))
+f2=int(input("Enter first rectangle second side: "))
+a1=f1*f2
+
+s1=int(input("Enter second rectangle first side: "))
+s2=int(input("Enter second rectangle second side:"))
+a2=s1*s2
+
+t1=int(input("Enter third rectangle first side: "))
+t2=int(input("Enter third rectangle second side: "))
+a3=t1*t2
+print("Area of first rectangle:",a1)
+print("Area of second rectangle:",a2)
+print("Area of third rectangle:",a3)
