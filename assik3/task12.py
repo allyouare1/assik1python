fc1f03a27bbbeb5edc7d47ec89c56559c55dd606
allyouare1 <242582@astanait.edu.kz>
2025-12-24 17:16:33 +0500

    sum,average

---
+++ assik3/task12.py
@@ -0,0 +1,20 @@
+arr1 = list(map(int, input("your first array: ").split()))
+arr2 = list(map(int, input("your second array: ").split()))
+arr3 = list(map(int, input("your third array: ").split()))
+s1=sum(arr1)
+print("sum of first:",s1)
+a1=s1/len(arr1)
+print("average of first:",a1)
+
+s2=sum(arr2)
+print("sum of second:",s2)
+a2=s2/len(arr2)
+print("average of second:",a2)
+
+s3=sum(arr3)
+print("sum of third:",s3)
+a3=s3/len(arr3)
+print("average of third:",a3)
+
+
+
