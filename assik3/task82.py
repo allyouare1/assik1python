fb5efd4976b939b51a361ff3027278a874581485
allyouare1 <242582@astanait.edu.kz>
2025-12-26 14:17:41 +0500

    array

---
+++ assik3/task82.py
@@ -0,0 +1,12 @@
+n=int(input())
+array = []
+for i in range(n):
+    x = int(input())
+    array.append(x)
+print("Initial array:", array)
+if n > 1:
+    temp = array[0]
+    array[0] = array[n - 1]
+    array[n - 1] = temp
+
+print("After array:", array)
